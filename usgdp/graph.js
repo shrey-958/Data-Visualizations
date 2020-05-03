94cbfd7b043a5b743feea2d2a866a40e8dbf47fd
shrey-958 <shreydedhia@yahoo.in>
2020-05-03 15:23:09 +0530

    Added some styling

--- usgdp/graph.js
+++ usgdp/graph.js
@@ -30,8 +30,10 @@ var gdp = []
 
 
 const xAxisGroup = graph.append('g')
-                                .attr('transform', `translate(0, ${graphHeight})`);
-        const yAxisGroup = graph.append('g');
+                                .attr('transform', `translate(0, ${graphHeight})`)
+                                .attr('id', 'x-axis');
+        const yAxisGroup = graph.append('g')
+            .attr('id', 'y-axis');
 d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json')
     .then(data => {
         data_main = data.data.map(d => d)
@@ -43,6 +45,7 @@ d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/mas
             })
             const tip = d3.tip()
     .attr('id', 'tooltip')
+    .attr('class', 'card')
     .html(d => {
         let content = `<div class="year">${d[0].split('-')[0]}</div>`
         var q = 0;
@@ -61,10 +64,11 @@ d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/mas
                 break;
         }
         content += `<div class="quarter">Q ${q}</div>`
-        content += `<div class="gdp_val">GDP - ${d[1]}</div>`
+        content += `<div class="gdp_val">GDP - $ ${d[1]} billion</div>`
 
         return content
     })
+    
 
 graph.call(tip)
 
@@ -93,19 +97,34 @@ graph.call(tip)
         }
       
           
+        graph.append('text')
+        .attr('class', 'gdp_text')
+        .text("Gross Domestic Product")
+        .attr('transform', 'rotate(-90)')
+        .attr('x', -350)
+        .attr('y', 40)
         
+       
         const rects = graph.selectAll('rects')
             .data(data_main)
 
             console.log(data_main.map(d => [d[0].split('-')[0], d[1]]))
             rects.attr('width', x.bandwidth)
             .attr('height', d => graphHeight - y(d[1]))
+            .attr('class', 'bar')
             .attr('fill', 'orange')
             .attr('x', d => x(d[0]))
             .attr('y', d => y(d[1]))
 
             rects.enter()
             .append('rect')
+            .attr('class', 'bar')
+            .attr('data-date', function(d, i) {
+                return data.data[i][0]
+              })
+              .attr('data-gdp', function(d, i) {
+                return data.data[i][1]
+              })
             .attr('width', graphWidth / data_main.length)
             .attr('height', d => graphHeight - y(d[1]))
             .attr('fill', 'orange')
@@ -130,13 +149,16 @@ graph.call(tip)
                     yAxisGroup.call(yAxis);
 
                     yAxisGroup.selectAll('text')
+                    .attr('fill', 'brown')
+                    .attr('stroke', 'brown')
                     .attr('font-size', 15);
                     xAxisGroup.selectAll('text')
                     .attr('transform', 'rotate(-20)')
 
                     .attr('text-anchor', 'end')
-                    .attr('fill', 'purple')
-                    .attr('font-size', 15);
+                    .attr('fill', 'brown')
+                    .attr('stroke', 'brown')
+                    .attr('font-size', 12);
         
 
         
